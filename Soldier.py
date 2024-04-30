59f3f1b79fe0da433ed43583b687239881a4bf0b
Nezumi-Remis <remiskamin@gmail.com>
2024-04-30 16:22:49 +0300

    Added Player Movement

--- Soldier.py
+++ Soldier.py
@@ -1,12 +1,36 @@
 import pygame
 
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
+    def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.char_type = char_type
+        self.__speed = speed
+        self.direction = 1
+        self.flip = False
+
         img = pygame.image.load('img/player/Idle/0.png')
         self.image = pygame.transform.scale(img, (int(img.get_width()*scale), int(img.get_height()*scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+    def move(self, moving_left, moving_right):
+        #reset movement variables
+        dx = 0
+        dy = 0
+
+        #assing movement variables
+        if moving_left:
+            dx = -self.__speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.__speed
+            self.flip = False
+            self.direction = 1
+
+        #update rectangle position
+        self.rect.x += dx
+        self.rect.y += dy
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
--- main.py
+++ main.py
@@ -10,20 +10,54 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Platformer Shooter')
 
-player = Soldier(200, 200, 3)
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define player action variables
+moving_left = False
+moving_right = False
+
+#define colours
+BG = (144, 201, 120)
+
+def draw_bg():
+	screen.fill(BG)
+
+player = Soldier ('player', 200, 200, 3, 5)
 
 #game loop/events
 run = True
 while run:
 
+    clock.tick(FPS)
+
+    draw_bg()
 
     player.draw(screen)
+    player.move(moving_left, moving_right)
+
 
     for event in pygame.event.get():
         #quit game
         if event.type == pygame.QUIT:
             run = False
-        
+        #keyboard presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+        #keivoard button realesed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+
+
         pygame.display.update()
 
 pygame.quit()
