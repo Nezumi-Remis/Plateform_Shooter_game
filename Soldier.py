294f9748f991a9378d8aaaec02a370b2d9a12f36
Nezumi-Remis <remiskamin@gmail.com>
2024-04-30 15:08:24 +0300

    Game Loop

---
+++ Soldier.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale, speed):
+        pass
+
+    def draw(self):
+        pass
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import pygame
+from Soldier import Soldier
+
+pygame.init()
+
+#screen parameters
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Platformer Shooter')
+
+
+#game loop/events
+run = True
+while run:
+
+    for event in pygame.event.get():
+        #quit game
+        if event.type == pygame.QUIT:
+            run = False
+        
+        pygame.display.update()
+
+pygame.quit()
