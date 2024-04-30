c83066409e683361313415edf32e3949c90fe354
Nezumi-Remis <remiskamin@gmail.com>
2024-04-30 15:15:47 +0300

    Added Image On Screen

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Platformer Shooter')
 
+player = Soldier(200, 200, 3)
 
 #game loop/events
 run = True
 while run:
+
+
+    player.draw(screen)
 
     for event in pygame.event.get():
         #quit game
